05a3d3dd6edb8b2d15b8e5cb7e18ccc5a13075c5
Hasher Amin <hasheramin.5@gmail.com>
2026-07-04 08:41:22 +0500

    Adding New Files

---
+++ 2. Conditions/a. Comparison Operators/1. Check if a Number is Positive .py
@@ -0,0 +1,12 @@
+# Check if a Number is Positive
+
+number = float(input("Enter a number: "))
+
+if number > 0:
+    print(number, "is a Positive Number")
+else:
+    print(number, "is not a Positive Number")
+
+
+# Explanation:
+# In this code snippet, we take a number as input from the user and convert it to a float. We then check if the number is greater than 0 using an if statement. If the condition is true, we print "Positive Number". This code helps in determining whether the entered number is positive or not.
