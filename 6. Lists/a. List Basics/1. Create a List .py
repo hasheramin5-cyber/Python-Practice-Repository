a33e4dfa3b4299f02da007f17122905fddabbe92
Hasher Amin <hasheramin.5@gmail.com>
2026-07-08 11:45:48 +0500

    Adding New Files

---
+++ 6. Lists/a. List Basics/1. Create a List .py
@@ -0,0 +1,8 @@
+# A Program to create a list.
+
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+print(fruits)
+
+# Explanation:
+# The program creates a list containing four fruit names. A list is created using square brackets [] and can store multiple values in a single variable. Lists are ordered and mutable, which means their elements can be changed after creation.
