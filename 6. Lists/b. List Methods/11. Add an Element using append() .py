e1246d3ecf7c5fadd8ba81bc534cba96a50c7e1f
Hasher Amin <hasheramin.5@gmail.com>
2026-07-08 11:48:53 +0500

    Adding New Files

---
+++ 6. Lists/b. List Methods/11. Add an Element using append() .py
@@ -0,0 +1,12 @@
+# A Program to add an element to the end of a list using the append() method.
+
+fruits = ["Apple", "Banana", "Mango"]
+
+fruit = input("\nEnter a fruit to add: ").title()
+
+fruits.append(fruit)
+
+print("Updated List =", fruits)
+
+# Explanation:
+# The program adds a new element to the end of the list using the append() method. This method inserts only one element at a time and modifies the original list.
