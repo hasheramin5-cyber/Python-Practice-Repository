054800cf2d578dd3434c98969030649e79804771
Hasher Amin <hasheramin.5@gmail.com>
2026-07-09 08:40:37 +0500

    Adding New Files

---
+++ 6. Lists/a. List Basics/2. Print a List .py
@@ -0,0 +1,10 @@
+# A Program to print all elements of a list.
+
+colors = ["Red", "Green", "Blue", "Yellow"]
+fruits = ["Apple", "Mango", "Banana", "Orange"]
+
+print("\nList No.1 : ", colors)
+print("List No.2 : ", fruits)
+
+# Explanation:
+# The program stores multiple color names inside a list and displays the entire list using the print() function. When a list is printed, Python displays all of its elements enclosed in square brackets.
