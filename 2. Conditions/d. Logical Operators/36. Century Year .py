f2b30a46bf5bb5e2cda01498e15feb2fe1f63a52
Hasher Amin <hasheramin.5@gmail.com>
2026-07-19 21:36:35 +0500

    docs(Conditions): Adding New Comments to Increase the Readability of User

--- 2. Conditions/d. Logical Operators/36. Century Year .py
+++ 2. Conditions/d. Logical Operators/36. Century Year .py
@@ -1,7 +1,14 @@
+# A Program to find whether the given year is a Century Year or not. Taking year input from the user and converting it into an integer
+
 year = int(input("Enter a year: "))
 
+# Checking if the year is completely divisible by 100
+# Years that are divisible by 100 are called Century Years
 if year % 100 == 0:
     print("Century Year")
 else:
     print("Not a Century Year")
+
+# Explanation:
+# A Century Year is a year that marks the beginning of a new century. It is always divisible by 100 without leaving any remainder. The modulus operator (%) is used to check the remainder after division. If the remainder is 0, the year is a Century Year; otherwise, it is not.
     
